ee8f2906fa71535e307200c5c319ae016cbe1535
Valentine <vp.mulia888@gmail.com>
2020-08-22 21:36:46 +0700

    error

--- app/models/event.rb
+++ app/models/event.rb
@@ -2,4 +2,7 @@ class Event < ApplicationRecord
   belongs_to :user
   has_many :appointments, dependent: :destroy
   has_one_attached :photo
+
+  $categories = ["Business", "Design", "Technology"]
+  $difficulties = ["Beginner", "Medium", "Difficult", "Expert"]
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,3 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+test_user = User.new(email: "tester@lewagon.com", fullname: "Chef", title: "User", password: "123456", expertise: "coding")
+test_user.save!
+puts "created new test user"
