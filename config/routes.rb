b2e0a7cf925d48e773ea9a22865287fc3441ca65
Valentine <vp.mulia888@gmail.com>
2020-08-24 13:30:05 +0700

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,16 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  resources :reviews
-  resources :appointments
-  resources :events
-
   devise_for :users
+  resources :reviews
+
+  resources :events do
+    resources :appointments, only: [:create]
+  end
+
+  resources :appointments, only: [:index, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+
   resources :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
