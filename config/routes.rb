3bd512d88291e2572cd8e3cb51b875d22ce81f24
Valentine <vp.mulia888@gmail.com>
2020-08-24 13:16:19 +0700

    all database works now

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  root to: 'pages#home'
   resources :reviews
   resources :appointments
   resources :events
 
   devise_for :users
   resources :users
-  root to: 'pages#home'
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20200822140000_reference.rb
+++
@@ -1,8 +0,0 @@
-class Reference < ActiveRecord::Migration[6.0]
-  def change
-    add_foreign_key :appointments, :events
-    add_foreign_key :appointments, :users, column: requester_id
-    add_foreign_key :events, :users
-    add_foreign_key :reviews, :appointments
-  end
-end
