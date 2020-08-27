3393ec3a40e8dd476e6e14491176f0e07342a8d1
Jasmine Huang <tinjasminee@gmail.com>
2020-08-27 12:32:01 +0800

    register funciton

---
+++ db/migrate/20200827034853_add_endtime_to_events.rb
@@ -0,0 +1,5 @@
+class AddEndtimeToEvents < ActiveRecord::Migration[6.0]
+  def change
+    add_column :events, :end_time, :datetime
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_22_135507) do
+ActiveRecord::Schema.define(version: 2020_08_27_034853) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -57,6 +57,7 @@ ActiveRecord::Schema.define(version: 2020_08_22_135507) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.datetime "date"
+    t.datetime "end_time"
     t.index ["user_id"], name: "index_events_on_user_id"
   end
 
